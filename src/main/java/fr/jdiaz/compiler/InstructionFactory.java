9861ea78a744ca20c3f8049cde39c85232e9aa1a
DIAZ Jérôme Olivier <j.diaz@eurelis.com>
2017-05-08 07:58:31 +0200

    we no longer use ScopeRange

--- src/main/java/fr/jdiaz/compiler/InstructionFactory.java
+++ src/main/java/fr/jdiaz/compiler/InstructionFactory.java
@@ -5,13 +5,6 @@ import fr.jdiaz.compiler.parser.Token;
 
 public interface InstructionFactory {
     
-    public boolean willPopContext();
-    public boolean willPushContext();
-    
-    public boolean willCreateConditionBranch();
-    public boolean willStartConditionBlock();
-    public boolean willEndConditionBlock();
-    
     public Instruction instantiate(Token token);
     
     public InstructionBranch getBranch(InstructionBranch branch, int index);
